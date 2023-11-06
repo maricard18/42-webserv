6dc9d008a1fe6ec1227f80879293e6dcd974cd46
maricard@student.42porto.com <maricard@student.42porto.com>
2023-11-06 21:35:22 +0000

    added argument on request.hpp function

--- miniserver/Request.hpp
+++ miniserver/Request.hpp
@@ -61,7 +61,7 @@ class Request
 		int			parseRequest(char* buffer, int bytesRead);
 		int			handleRequest(char* buffer, int bytesRead);
 		void		handleBody(char* buffer, int bytesRead);
-		int			isValidRequest(Server& server);
+		int			isValidRequest(Server& server, std::string& response);
 		std::string	runCGI();
 
 		void	setArgv();
